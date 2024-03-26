1f9c839710cb2b5d4f09dcbe66abc5e445845dd2
Chandrakantgit <chandrakhannade@gmail.com>
2024-03-26 22:28:00 +0530

    learned print statements

--- hello.rs
+++ hello.rs
@@ -1,4 +1,15 @@
 fn main(){
     println!("Hello World!"); // println is a macro here,its not a fucntion.Macors in Rust allows extensive metaprogramming
+    let x = 5;
+    println!("{}",x+5+5);
 
+        // As can named arguments.
+        println!("{subject} {verb} {object}",
+        object="the lazy dog",
+        subject="the quick brown fox",
+        verb="jumps over");
+
+    let number: f64 = 1.0;
+    let width: usize = 5;
+    println!("{number:>width$}");
 }
